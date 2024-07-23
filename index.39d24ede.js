f1075a6e2c29560da86c7c4f6ce5d08e18b47eb5
my-git-username <118355685+my-git-username@users.noreply.github.com>
2024-07-23 13:17:39 +0300

    make build

---
+++ index.39d24ede.js
@@ -0,0 +1,2 @@
+document.body.id="tree",function e(n,t){var r=document.createElement("ul");for(var a in t){var d=document.createElement("li");r.appendChild(d),d.textContent=a,Object.keys(t[a]).length>0&&e(d,t[a])}n.appendChild(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.39d24ede.js.map
